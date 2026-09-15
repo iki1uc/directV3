42eeb483c59b4d5803982881bdfedbb9c8ed79ed
iki1uc <entertrainer@web.de>
2026-09-15 09:01:58 +0200

    Update room.js

--- room.js
+++ room.js
@@ -1,1 +1,11 @@
-
+export const ROOM = {
+  name: "ANKER Room",
+  innen: true, außen: false, // Warum immer von außen? Jetzt drinne!
+  wände: 12 Achsen = 12 Kanten Raum drinne
+  pyramideDrinne: {
+    basis: T0 LIVE6D-Basis Grundfläche drinne
+    seiten: T1 Morph, T2 Mittelwert Harmonie, T3 Differenz, T4 Norm Cache 4x = Seiten drinne
+    spitze: T5 Hierarchie 9-3-27-81 = Spitze drinne 128X
+    blüht: false -> blühenLassen() = 🌸 Pyramide drinne blüht!
+  }
+};
